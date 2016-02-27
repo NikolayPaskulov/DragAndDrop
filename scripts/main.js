da01678f85386f12ea2b07e5a65181c75e88d7ea
Nikolay Paskulov <t3am0wner@gmail.com>
2016-02-27 20:30:39 +0200

    add components layout interaction.

--- scripts/main.js
+++ scripts/main.js
@@ -1,49 +1,14 @@
+
 $(function () {
 
     var ComponentsInteractions = (function () {
-        
         var selectors = {
             pageLayout: '#page-layout',
-            components : '#components',
+            components: '#components',
             componentsHeader: '#components .components-header',
-            componentsMinimize: '#components .components-header .components-minimize',
-            componentsPin: '#components .components-header .components-pin'
         }
 
-        var events = {
-            pin: {
-                click: function (e) {
-                    var _this = $(this);
-                    if (_this.data('pinned') === true) {
-                        var components = $(selectors.components);
-                        var direction = components.data('direction')
-                        var componentsWidth = components.width();
-
-                        components.css('width', componentsWidth);
-
-                        if (direction == 'left') {
-                            components.css('right', '10px');
-                        } else if (direction == 'right') {
-                            components.css('left', '10px');
-                        } else if (direction == 'top') {
-                            components.css('top', '10px');
-                        } else if (direction == 'bottom') {
-                            components.css('bottom', '10px');
-                        }
-
-                        components.addClass("fixed");
-
-
-                        _this.data('isPinned', false);
-                    } else {
-                        var components = $(selectors.components);
-                        var direction = components.data('direction');
-
-                        _this.data('isPinned', true);
-                    }
-                }
-            }
-        }
+        var Directions = { Left: 'left', Right: 'right' };
 
         function ComponentsInteractions() {
             this.Initialize();
@@ -54,12 +19,103 @@ $(function () {
         };
 
         ComponentsInteractions.prototype.AddEvents = function () {
-            $(selectors.componentsPin).on("click", events.pin.click)
+            this.AddComponentsEvents();
         };
 
-        // helpers
+        ComponentsInteractions.prototype.AddComponentsEvents = function () {
+            dragula([document.querySelector('.container-fluid > .row')], {
+                moves: function (el, source, handle, sibling) {
+                    return el.id != 'page-layout' && handle.className == 'components-header';
+                },
+                accepts: function (el, target, source, sibling) {
+                    return false;
+                },
+            })
+            .on('drag', drag_start)
+            .on('dragend', drag_end);
 
+            function drag_start(element) {
+                // Create a shadow
 
+                // handle on mouse move
+                // update data-direction
+                // update shadow position
+
+                var el = $(element);
+                var direction = el.data('direction');
+
+                var dropShadow = $('<div />')
+                                    .addClass('components-drop-shadow')
+                                    .hide()
+                                    .data('direction', direction);
+
+                $(document).on('mousemove', function (ev) {
+                    var closeToEdge = 20;
+                    var width = $(document.body).width();
+                    var height = $(document.body).height();
+                    var x = ev.pageX;
+                    var y = ev.pageY;
+
+                    if (x >= (width - closeToEdge)) {
+                        positionShadow(Directions.Right, el.width() + 50);
+                    } else if (x <= closeToEdge) {
+                        positionShadow(Directions.Left, el.width() + 50);
+                    } else {
+                        resetShadow();
+                    }
+                });
+
+                function resetShadow() {
+                    dropShadow.removeClass(dropShadow.data('direction'));
+                    dropShadow.data('direction', false);
+                    dropShadow.hide();
+                }
+
+                function positionShadow(direction, width) {
+                    var directionClass = dropShadow.data('direction');
+
+                    dropShadow.removeClass(directionClass);
+                    dropShadow.addClass(direction);
+
+                    if (width)
+                        dropShadow.css('width', width);
+
+                    dropShadow.data('direction', direction);
+                    dropShadow.show();
+                }
+
+                $('.container-fluid').append(dropShadow);
+            }
+
+            function drag_end(element) {
+                // handle drop based on data-direction
+                // remove drop-shadow
+
+                var el = $(element);
+                var pageLayout = $(selectors.pageLayout);
+                var shadow = $('.components-drop-shadow');
+                var direction = shadow.data('direction');
+
+                if (direction != false && direction != el.data('direction')) {
+                    if (direction == Directions.Left) {
+                        replaceComeponets(Directions.Left, 'col-xs-3', 'col-xs-9');
+                    } else if (direction == Directions.Right) {
+                        replaceComeponets(Directions.Right, 'col-xs-3 col-xs-push-9', 'col-xs-9 col-xs-pull-3');
+                    }
+                }
+
+                function replaceComeponets(direction, componentsClasses, pageClasses) {
+                    el.get(0).className = componentsClasses;
+                    pageLayout.get(0).className = pageClasses;
+
+                    el.data('direction', direction);
+                }
+
+                $(document).off('mousemove');
+
+                shadow.remove();
+            }
+        };
 
         return ComponentsInteractions;
     })();
